cb03d90772968cb2824f8559b9ac823e8385f526
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2022-02-21 09:18:39 +0100

    Added chase range to enemy. It will go back to starting pos when player goes out of range.

--- Assets/Scripts/EnemyMover.cs
+++ Assets/Scripts/EnemyMover.cs
@@ -6,17 +6,34 @@ using UnityEngine.AI;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 7.0f;
+
+
     private NavMeshAgent navMeshAgent;
+    private float distanceToTargetSqr = Mathf.Infinity;
+    private Vector3 startPos;
+
 
     // Start is called before the first frame update
     void Start()
     {
         this.navMeshAgent = this.gameObject.GetComponent<NavMeshAgent>();
+
+        this.startPos = this.gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.navMeshAgent.SetDestination(this.target.position);
+        this.distanceToTargetSqr = (this.target.transform.position - this.gameObject.transform.position).sqrMagnitude;
+
+        if(this.distanceToTargetSqr <= (this.chaseRange*this.chaseRange))
+        {
+            this.navMeshAgent.SetDestination(this.target.position);
+        }
+        else if(!Vector3.Equals(this.distanceToTargetSqr, this.startPos))
+        {
+            this.navMeshAgent.SetDestination(this.startPos);
+        }
     }
 }

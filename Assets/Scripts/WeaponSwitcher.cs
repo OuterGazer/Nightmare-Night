e8dd401a8af68ca4f8f1833c02b4a527ab3bf343
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2022-03-25 12:43:05 +0100

    Implemented weapon change with keyboard number press.

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -1,15 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WeaponSwitcher : MonoBehaviour
 {
+    [Header("Button Mapping")]
+    [SerializeField] InputAction weaponSelect;
+
+    [Header("Misc.")]
     [SerializeField] int currentWeapon = 0;
+
+    private void Awake()
+    {
+        this.weaponSelect.Enable();
+    }
+
+    private void OnDestroy()
+    {
+        this.weaponSelect.Disable();
+    }
 
     // Start is called before the first frame update
     void Start()
     {
         SetWeaponActive();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int previousWeapon = this.currentWeapon;
+
+        ProcessKeyInput();
+        ProcessScrollWheel();
+
+        if (previousWeapon != this.currentWeapon)
+            SetWeaponActive();
+    }    
+
+    private void ProcessKeyInput()
+    {
+        if (this.weaponSelect.triggered)
+        {
+            switch (this.weaponSelect.activeControl.displayName)
+            {
+                case string i when i.Contains("1"):
+                    this.currentWeapon = 0;
+                    break;
+
+                case string i when i.Contains("2"):
+                    this.currentWeapon = 1;
+                    break;
+            }
+        }
+    }
+
+    private void ProcessScrollWheel()
+    {
+        
     }
 
     private void SetWeaponActive()
@@ -29,11 +79,5 @@ public class WeaponSwitcher : MonoBehaviour
 
             weaponIndex++;
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }

913a02e7dae6981ff36ea2ba9f7751b71b361871
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2022-05-10 14:08:08 +0200

    Implemented start menu funtionality to start the game.

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -7,6 +7,9 @@ public class MenuController : MonoBehaviour
 {
     [SerializeField] PlayerInput playerInput;
     [SerializeField] PlayerHealth playerHealth;
+    [SerializeField] GameObject startCollider;
+
+    [SerializeField] Transform startPoint;
 
     private void Awake()
     {
@@ -14,12 +17,29 @@ public class MenuController : MonoBehaviour
         this.playerHealth.enabled = false;
     }
 
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void OnClickStart()
     {
-        this.gameObject.transform.position = new Vector3(82.1289978f, 5.8912487f, 30.4769993f);
+        GameObject.Destroy(this.startCollider);
+
+        this.playerHealth.GetComponent<CharacterController>().enabled = false;
+
+        GameObject player = this.playerHealth.gameObject;
+
+        player.transform.position = startPoint.position;
+        player.transform.rotation = Quaternion.Euler(new Vector3(0.0f, 15.0f, 0.0f));
+
+        this.playerHealth.GetComponent<CharacterController>().enabled = true;
 
         this.playerInput.enabled = true;
         this.playerHealth.enabled = true;
+
+        this.gameObject.SetActive(false);
     }
 
     public void OnClickCredits()

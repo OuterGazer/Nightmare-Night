ab29bb2f8a00727fb24dd2bff3cde0e996350d04
Aythami Cabrera Rodriguez <aythami.cabrera@gmail.com>
2022-05-15 09:12:54 +0200

    Implemented SFX for healing and ammo pickup.

--- Assets/Scripts/AmmoPickup.cs
+++ Assets/Scripts/AmmoPickup.cs
@@ -7,13 +7,22 @@ public class AmmoPickup : MonoBehaviour
     [SerializeField] AmmoType ammoType;
     [SerializeField] int ammoAmount = default;
 
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        this.audioSource = this.gameObject.GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             Ammo ammo = GameObject.FindObjectOfType<Ammo>();
 
-            if (!ammo.AddAmmo(this.ammoType, this.ammoAmount)) { return; }            
+            if (!ammo.AddAmmo(this.ammoType, this.ammoAmount)) { return; }
+
+            AudioSource.PlayClipAtPoint(this.audioSource.clip, Camera.main.transform.position, this.audioSource.volume);
 
             GameObject.Destroy(this.gameObject);
         }
--- Assets/Scripts/HealthPickup.cs
+++ Assets/Scripts/HealthPickup.cs
@@ -6,16 +6,44 @@ public class HealthPickup : MonoBehaviour
 {
     [SerializeField] float amountToHeal = default;
 
+    [SerializeField] AudioClip openBottle;
+    [SerializeField] AudioClip reliefSFX;
+
+    private AudioSource audioSource;
+    private Collider potionCol;
+    private MeshRenderer potionMesh;
+
+    private void Awake()
+    {
+        this.audioSource = this.gameObject.GetComponent<AudioSource>();
+        this.potionCol = this.gameObject.GetComponent<MeshCollider>();
+        this.potionMesh = this.gameObject.GetComponentInChildren<MeshRenderer>();
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             if (other.GetComponent<PlayerHealth>().AddHealth(this.amountToHeal))
             {
-                // TODO: add SFX
+                this.StartCoroutine(PlayDrinkSFX());
 
-                GameObject.Destroy(this.gameObject);
+                this.potionCol.enabled = false;
+                this.potionCol.enabled = false;
             }
         }
+    }
+
+    private IEnumerator PlayDrinkSFX()
+    {
+        this.audioSource.PlayOneShot(this.openBottle);
+
+        yield return new WaitUntil(() => !this.audioSource.isPlaying);
+
+        this.audioSource.PlayOneShot(this.reliefSFX);
+
+        yield return new WaitUntil(() => !this.audioSource.isPlaying);
+
+        GameObject.Destroy(this.gameObject);
     }
 }
